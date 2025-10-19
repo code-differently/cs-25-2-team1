2e22358743db8fd2ca054d21ed1684bc2647d21f
Kerry Ferguson <112345520+KerryFerguson04@users.noreply.github.com>
2025-10-19 17:14:18 -0400

    feat: adds component #7 ToDoList to the homepage (#32)
    * feat: adds component #2 to the dashboard page
    
    * adds component #3 to the dashboard page. Fixes button in component #2 as well.
    
    * feat: feat: adds component #4 to the dashboard page
    
    * feat: Adds components #5 and #6 TasksandReminders to the dashboard page. Also deletes duplicate import.
    
    * feat: adds component #7 ToDoList to the dashboard page.

--- habitTrackerApp/src/app/page.tsx
+++ habitTrackerApp/src/app/page.tsx
@@ -1,5 +1,6 @@
 import ProgressTracker from "./components/progress-tracker";
 import TasksAndReminders from "./components/tasks-and-reminders";
+import ToDoList from "./components/todo-list";
 import WeeklyStreak from "./components/weekly-streak";
 import { Welcome } from "./components/welcome";
 
@@ -10,9 +11,10 @@ export default function HomePage() {
       <div className="mt-8">
         <WeeklyStreak />
       </div>
-      <div className="mt-8 flex gap-24 items-start">
+      <div className="mt-8 flex gap-24 items-start ml-8">
         <ProgressTracker />
         <TasksAndReminders />
+        <ToDoList />
       </div>
     </div>
   );
