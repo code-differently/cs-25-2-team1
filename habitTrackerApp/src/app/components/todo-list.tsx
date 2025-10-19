cf46bb4c5351db5c6f67c76471660656aadd1e57
Kerry Ferguson <kferg2021@gmail.com>
2025-10-19 20:46:14 +0000

    feat: adds component #7 ToDoList to the dashboard page.

---
+++ habitTrackerApp/src/app/components/todo-list.tsx
@@ -0,0 +1,13 @@
+
+const ToDoList = () => {
+  return (
+    <div className="flex flex-col w-64">
+      {/* To Do List Card */}
+      <div className="flex items-start justify-center py-6 px-8 bg-gray-200 rounded-3xl h-96">
+        <h3 className="text-lg font-semibold text-black">To Do List</h3>
+      </div>
+    </div>
+  );
+};
+
+export default ToDoList;
