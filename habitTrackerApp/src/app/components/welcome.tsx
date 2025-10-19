4b541d07683c0089e9d736d40c7c78e022e03815
Kerry Ferguson <kferg2021@gmail.com>
2025-10-19 17:42:29 +0000

    adds component #3 to the dashboard page. Fixes button in component #2 as well.

--- habitTrackerApp/src/app/components/welcome.tsx
+++ habitTrackerApp/src/app/components/welcome.tsx
@@ -17,7 +17,7 @@ export const Welcome: FC<WelcomeProps> = ({ userName = "Jane", className = "" })
       <div className="flex items-center gap-4">
         {/* Add Task Button */}
         <button className="px-8 py-2 text-white text-base font-medium bg-blue-500 rounded-full hover:bg-blue-600 transition-colors">
-          Add Task
+          Add Habit
         </button>
         
         {/* Journal Button */}
