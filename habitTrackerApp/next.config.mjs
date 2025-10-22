dda53fe9164c4ebcd35f7b2124defea155e5bdf8
emblue252 <emblue252@gmail.com>
2025-10-22 20:51:35 +0000

    feat: tries optomizecss again

--- habitTrackerApp/next.config.mjs
+++ habitTrackerApp/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   output: 'standalone',
   experimental: {
     optimizePackageImports: ['react', 'react-dom'],
+    optimizeCss: false,
   },
   webpack: (config) => {
     config.resolve.alias = {
